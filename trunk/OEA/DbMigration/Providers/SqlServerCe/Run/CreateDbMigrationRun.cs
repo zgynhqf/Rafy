0a478a095c21ca17bbcc255b5f169441836d0e75
huqf <huqf@567c7cce-0dd1-4c3e-9078-2c2066d777e5>
2012-04-25 03:09:36 +0000

    2.11.590.0     内部修改         * Web 支持 SQLCE 配置。

--- trunk/OEA/DbMigration/Providers/SqlServerCe/Run/CreateDbMigrationRun.cs
+++ trunk/OEA/DbMigration/Providers/SqlServerCe/Run/CreateDbMigrationRun.cs
@@ -32,12 +32,45 @@ namespace DbMigration.SqlServerCe
         {
             //保存目录存在。
             var csb = new SqlCeConnectionStringBuilder(db.Connection.ConnectionString);
-            var dir = Path.GetDirectoryName(csb.DataSource);
+            var path = CommonUtils.ReplaceDataDirectory(csb.DataSource);
+            var dir = Path.GetDirectoryName(path);
             Directory.CreateDirectory(dir);
 
             //调用引擎创建数据库
             var engine = new SqlCeEngine(db.Connection.ConnectionString);
             engine.CreateDatabase();
+        }
+
+        /// <summary>
+        /// 本类完全 Copy 自 System.Data.SqlServerCe 程序集
+        /// </summary>
+        internal static class CommonUtils
+        {
+            private const string DataDirectoryMacro = "|DataDirectory|";
+            private const string DataDirectory = "DataDirectory";
+            public static string ReplaceDataDirectory(string inputString)
+            {
+                string result = inputString.Trim();
+                if (!string.IsNullOrEmpty(inputString) && inputString.StartsWith(DataDirectoryMacro, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    string text = AppDomain.CurrentDomain.GetData(DataDirectory) as string;
+                    if (string.IsNullOrEmpty(text))
+                    {
+                        text = (AppDomain.CurrentDomain.BaseDirectory ?? Environment.CurrentDirectory);
+                    }
+                    if (string.IsNullOrEmpty(text))
+                    {
+                        text = string.Empty;
+                    }
+                    int num = DataDirectoryMacro.Length;
+                    if (inputString.Length > DataDirectoryMacro.Length && '\\' == inputString[DataDirectoryMacro.Length])
+                    {
+                        num++;
+                    }
+                    result = Path.Combine(text, inputString.Substring(num));
+                }
+                return result;
+            }
         }
     }
 }
