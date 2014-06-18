bee215e28ece561b47624a41bcbb820b5516ca97
huqf <huqf@567c7cce-0dd1-4c3e-9078-2c2066d777e5>
2014-06-18 05:49:41 +0000

    Rafy 2.28.2420

--- Demo/JXC/Web/Purchase/Commands/PurchaseOrderItemCommands.js
+++
@@ -1,9 +0,0 @@
-﻿Ext.define('Jxc.AddPurchaseOrderItem', {
-    extend: 'Jxc.SelectProductCommand',
-    config: { meta: { text: "选择商品", group: "edit" } },
-
-    //proected override
-    onItemSelected: function (product, newItem) {
-        newItem.set('RawPrice', product.get('CaiGouDanjia'));
-    }
-});
