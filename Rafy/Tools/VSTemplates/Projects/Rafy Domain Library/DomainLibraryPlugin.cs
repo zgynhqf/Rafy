455edd37f06baab5964adddedc8241d018893bab
deepali2009 <deepali2009@users.noreply.github.com>
2014-06-29 06:56:41 +0000

    2.29.2451

--- Rafy/Tools/VSTemplates/Projects/Rafy Domain Library/DomainLibraryPlugin.cs
+++ Rafy/Tools/VSTemplates/Projects/Rafy Domain Library/DomainLibraryPlugin.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Rafy;
+using Rafy.ComponentModel;
 using Rafy.DbMigration;
 using Rafy.Domain;
 using Rafy.Domain.ORM.DbMigration;
