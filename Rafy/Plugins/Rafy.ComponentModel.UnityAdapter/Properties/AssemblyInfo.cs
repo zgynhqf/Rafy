28afad67bb0e6e0e1dcb782d8609d225015b07e4
Huqf <9474649@qq.com>
2016-05-02 03:26:26 +0800

    提交最新的版本号。

--- Rafy/Plugins/Rafy.ComponentModel.UnityAdapter/Properties/AssemblyInfo.cs
+++ Rafy/Plugins/Rafy.ComponentModel.UnityAdapter/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.48.3095")]
+[assembly: AssemblyVersion("3.51.3140")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
--- Rafy/WPF/Rafy.WPF/Properties/AssemblyInfo.cs
+++ Rafy/WPF/Rafy.WPF/Properties/AssemblyInfo.cs
@@ -40,7 +40,7 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.48.3095")]
+[assembly: AssemblyVersion("3.51.3140")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.SourceAssembly,
