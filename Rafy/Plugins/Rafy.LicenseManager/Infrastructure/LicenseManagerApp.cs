5315005dcb3ba5ef204318e3c96870a261a5a8e9
wuzp <315136798@qq.com>
2016-10-11 14:14:39 +0800

    软加密 (#4)
    * 提交加密授权
    
    * 修改注释
    
    * 更改类为public
    
    * init license tool.
    
    * add ignore item.
    
    * validate input value.
    
    * 增加版本文件
    
    * 更改目录
    
    * Completed LicenseManager development.
    
    * ignore Debug and obj directory.
    
    * ignore obj directory.
    
    * relector reference library.
    
    * modity ignore file.
    
    * modity ignore file.
    
    * 更改frmaework 版本为4.5
    
    * completed License show into DataGridView control.
    
    * add DescriptionConvert.cs
    
    * 修改方法为静态
    
    * 修改工程文件目录
    
    * Add comments.
    
    * override CanConvertTo method.
    
    * a little modify.
    
    * 添加 rafy.licensemanager 使用说明
    
    * 去掉注释的空格

---
+++ Rafy/Plugins/Rafy.LicenseManager/Infrastructure/LicenseManagerApp.cs
@@ -0,0 +1,48 @@
+﻿/*******************************************************
+ * 
+ * 作者：宋军瑞
+ * 创建日期：20160921
+ * 说明：此文件只包含一个类，具体内容见类型注释。
+ * 运行环境：.NET 4.5
+ * 版本号：1.0.0
+ * 
+ * 历史记录：
+ * 创建文件 宋军瑞 20160921 10:00
+ * 
+*******************************************************/
+
+using Rafy.Domain;
+
+namespace Rafy.LicenseManager.Infrastructure
+{
+    internal class LicenseManagerApp : DomainApp
+    {
+        protected override void InitEnvironment()
+        {
+            RafyEnvironment.Provider.IsDebuggingEnabled = ConfigurationHelper.GetAppSettingOrDefault("IsDebuggingEnabled", false);
+
+            RafyEnvironment.DomainPlugins.Add(new LicenseManagerPlugin());
+
+            base.InitEnvironment();
+        }
+
+        //internal static void GeneratorPublicPrivateKey()
+        //{
+        //    var dir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data");
+
+        //    if(!Directory.Exists(dir))
+        //        Directory.CreateDirectory(dir);
+
+        //    var privateKeyPath = Path.Combine(dir, "private.bin");
+        //    var publicKeyPath = Path.Combine(dir, "public.bin");
+
+        //    if(File.Exists(privateKeyPath) && File.Exists(publicKeyPath))
+        //        return;
+
+        //    var keys = RSACryptoService.GenerateKeys();
+
+        //    File.WriteAllText(privateKeyPath, keys[0], Encoding.UTF8);
+        //    File.WriteAllText(publicKeyPath, keys[1], Encoding.UTF8);
+        //}
+    }
+}
---
+++ Rafy/Plugins/Rafy.LicenseManager/LicenseManagerPlugin.cs
@@ -0,0 +1,42 @@
+﻿/*******************************************************
+ * 
+ * 作者：宋军瑞
+ * 创建日期：20160921
+ * 说明：此文件只包含一个类，具体内容见类型注释。
+ * 运行环境：.NET 4.5
+ * 版本号：1.0.0
+ * 
+ * 历史记录：
+ * 创建文件 宋军瑞 20160921 10:00
+ * 
+*******************************************************/
+
+using System;
+using Rafy.ComponentModel;
+using Rafy.DbMigration;
+using Rafy.Domain;
+using Rafy.Domain.ORM.DbMigration;
+using Rafy.LicenseManager.Entities;
+
+namespace Rafy.LicenseManager
+{
+    public class LicenseManagerPlugin : DomainPlugin
+    {
+        public override void Initialize(IApp app)
+        {
+            app.RuntimeStarting += this._App_RuntimeStarting;
+        }
+
+        private void _App_RuntimeStarting(object sender, EventArgs e)
+        {
+            var svc = ServiceFactory.Create<MigrateService>();
+            svc.Options = new MigratingOptions
+            {
+                //ReserveHistory = true,//ReserveHistory 表示是否需要保存所有数据库升级的历史记录
+                RunDataLossOperation = DataLossOperation.All,//要支持数据库表、字段的删除操作，取消本行注释。
+                Databases = new string[] { LicenseManagerEntityRepository.DbSettingName }
+            };
+            svc.Invoke();
+        }
+    }
+}
