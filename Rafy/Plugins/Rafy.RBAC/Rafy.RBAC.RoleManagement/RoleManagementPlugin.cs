1d8cfec98d9a9f53665d1b2ae5387a9950a62934
Huqf <9474649@qq.com>
2016-11-29 15:04:28 +0800

    添加新版本 RBAC 三个插件，准备开发新版本。

---
+++ Rafy/Plugins/Rafy.RBAC/Rafy.RBAC.RoleManagement/RoleManagementPlugin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rafy;
+using Rafy.ComponentModel;
+using Rafy.DbMigration;
+using Rafy.Domain;
+using Rafy.Domain.ORM.DbMigration;
+
+namespace Rafy.RBAC.RoleManagement
+{
+    public class RoleManagementPlugin : DomainPlugin
+    {
+        public static string DbSettingName = "RoleManagement";
+
+        public override void Initialize(IApp app)
+        {
+        }
+    }
+}
---
+++ Rafy/Plugins/Rafy.RBAC/Rafy.RBAC.UserRoleManagement/UserRoleManagementPlugin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rafy;
+using Rafy.ComponentModel;
+using Rafy.DbMigration;
+using Rafy.Domain;
+using Rafy.Domain.ORM.DbMigration;
+
+namespace Rafy.RBAC.UserRoleManagement
+{
+    public class UserRoleManagementPlugin : DomainPlugin
+    {
+        public static string DbSettingName = "UserRoleManagement";
+
+        public override void Initialize(IApp app)
+        {
+        }
+    }
+}
