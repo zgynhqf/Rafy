f94677852a7fcf8dccf2b1a60b2cb9a7678a99d1
liulei <ll_efort@sina.com>
2016-11-30 16:54:17 +0800

    提交实体

---
+++ Rafy/Plugins/Rafy.RBAC/Rafy.RBAC.GroupManagement/Entities/Group.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Permissions;
+using System.Text;
+using Rafy;
+using Rafy.ComponentModel;
+using Rafy.Data;
+using Rafy.Domain;
+using Rafy.Domain.ORM;
+using Rafy.Domain.ORM.Query;
+using Rafy.Domain.Validation;
+using Rafy.ManagedProperty;
+using Rafy.MetaModel;
+using Rafy.MetaModel.Attributes;
+using Rafy.MetaModel.View;
+
+namespace Rafy.RBAC.GroupManagement
+{
+    /// <summary>
+    /// 用户组
+    /// </summary>
+    [RootEntity, Serializable]
+    public partial class Group : GroupManagementEntity
+    {
+        #region 构造函数
+
+        public Group() { }
+
+        [SecurityPermissionAttribute(SecurityAction.Demand, SerializationFormatter = true)]
+        protected Group(SerializationInfo info, StreamingContext context) : base(info, context) { }
+
+        #endregion
+
+        #region 引用属性
+
+        #endregion
+
+        #region 组合子属性
+
+        public static readonly ListProperty<GroupRoleList> GroupRoleListProperty = P<Group>.RegisterList(e => e.GroupRoleList);
+        public GroupRoleList GroupRoleList
+        {
+            get { return this.GetLazyList(GroupRoleListProperty); }
+        }
+
+        public static readonly ListProperty<GroupUserList> GroupUserListProperty = P<Group>.RegisterList(e => e.GroupUserList);
+        public GroupUserList GroupUserList
+        {
+            get { return this.GetLazyList(GroupUserListProperty); }
+        }
+
+        #endregion
+
+        #region 一般属性
+
+        #endregion
+
+        #region 只读属性
+
+        #endregion
+    }
+
+    /// <summary>
+    /// 用户组 列表类。
+    /// </summary>
+    [Serializable]
+    public partial class GroupList : GroupManagementEntityList { }
+
+    /// <summary>
+    /// 用户组 仓库类。
+    /// 负责 用户组 类的查询、保存。
+    /// </summary>
+    public partial class GroupRepository : GroupManagementEntityRepository
+    {
+        /// <summary>
+        /// 单例模式，外界不可以直接构造本对象。
+        /// </summary>
+        protected GroupRepository() { }
+    }
+
+    /// <summary>
+    /// 用户组 配置类。
+    /// 负责 用户组 类的实体元数据的配置。
+    /// </summary>
+    internal class GroupConfig : GroupManagementEntityConfig<Group>
+    {
+        /// <summary>
+        /// 配置实体的元数据
+        /// </summary>
+        protected override void ConfigMeta()
+        {
+            //配置实体的所有属性都映射到数据表中。
+            Meta.MapTable().MapAllProperties();
+        }
+    }
+}
---
+++ Rafy/Plugins/Rafy.RBAC/Rafy.RBAC.GroupManagement/Entities/GroupRole.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Permissions;
+using System.Text;
+using Rafy;
+using Rafy.ComponentModel;
+using Rafy.Data;
+using Rafy.Domain;
+using Rafy.Domain.ORM;
+using Rafy.Domain.ORM.Query;
+using Rafy.Domain.Validation;
+using Rafy.ManagedProperty;
+using Rafy.MetaModel;
+using Rafy.MetaModel.Attributes;
+using Rafy.MetaModel.View;
+
+namespace Rafy.RBAC.GroupManagement
+{
+    /// <summary>
+    /// 组和角色映射
+    /// </summary>
+    [ChildEntity, Serializable]
+    public partial class GroupRole : GroupManagementEntity
+    {
+        #region 构造函数
+
+        public GroupRole() { }
+
+        [SecurityPermissionAttribute(SecurityAction.Demand, SerializationFormatter = true)]
+        protected GroupRole(SerializationInfo info, StreamingContext context) : base(info, context) { }
+
+        #endregion
+
+        #region 引用属性
+
+        public static readonly IRefIdProperty GroupIdProperty =
+            P<GroupRole>.RegisterRefId(e => e.GroupId, ReferenceType.Parent);
+
+        /// <summary>
+        /// 组的主键
+        /// </summary>
+        public int GroupId
+        {
+            get { return (int)this.GetRefId(GroupIdProperty); }
+            set { this.SetRefId(GroupIdProperty, value); }
+        }
+        public static readonly RefEntityProperty<Group> GroupProperty =
+            P<GroupRole>.RegisterRef(e => e.Group, GroupIdProperty);
+
+        /// <summary>
+        /// 关联的组对象
+        /// </summary>
+        public Group Group
+        {
+            get { return this.GetRefEntity(GroupProperty); }
+            set { this.SetRefEntity(GroupProperty, value); }
+        }
+
+
+        #endregion
+
+        #region 组合子属性
+
+        #endregion
+
+        #region 一般属性
+
+        #endregion
+
+        #region 只读属性
+
+        #endregion
+    }
+
+    /// <summary>
+    /// 组和角色映射 列表类。
+    /// </summary>
+    [Serializable]
+    public partial class GroupRoleList : GroupManagementEntityList { }
+
+    /// <summary>
+    /// 组和角色映射 仓库类。
+    /// 负责 组和角色映射 类的查询、保存。
+    /// </summary>
+    public partial class GroupRoleRepository : GroupManagementEntityRepository
+    {
+        /// <summary>
+        /// 单例模式，外界不可以直接构造本对象。
+        /// </summary>
+        protected GroupRoleRepository() { }
+    }
+
+    /// <summary>
+    /// 组和角色映射 配置类。
+    /// 负责 组和角色映射 类的实体元数据的配置。
+    /// </summary>
+    internal class GroupRoleConfig : GroupManagementEntityConfig<GroupRole>
+    {
+        /// <summary>
+        /// 配置实体的元数据
+        /// </summary>
+        protected override void ConfigMeta()
+        {
+            //配置实体的所有属性都映射到数据表中。
+            Meta.MapTable().MapAllProperties();
+        }
+    }
+}
