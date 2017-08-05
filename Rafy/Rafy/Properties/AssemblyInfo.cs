56f38c9568de2651fadc3739a44d302bc91c24a0
Huqf <9474649@qq.com>
2017-08-05 17:27:47 +0800

    NetStandard：适配 Solution 和 Project 文件。

--- Rafy/Rafy/Properties/AssemblyInfo.cs
+++ Rafy/Rafy/Properties/AssemblyInfo.cs
@@ -5,14 +5,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Rafy")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hxy Team")]
-[assembly: AssemblyProduct("Rafy")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+//[assembly: AssemblyTitle("Rafy")]
+//[assembly: AssemblyDescription("")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyCompany("Hxy Team")]
+//[assembly: AssemblyProduct("Rafy")]
+//[assembly: AssemblyCopyright("Copyright ©  2011")]
+//[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.51.3140")]
+//[assembly: AssemblyVersion("3.51.3140")]
 
 [assembly: InternalsVisibleTo("Rafy.WPF, PublicKey=0024000004800000940000000602000000240000525341310004000001000100d3ae8c70610ec4e9ddd1c83c90c9e99c19a4c675e79b0360283797b567ea20e1a6804e1bd5459f5ad6d354a3ee5d1608faafff8d33460116af32c3e9b012c38f5ef334eff3b7f85f1a3db52e4f44ebbbf24429b5650072bb1968ea80dec1b97ab9429db053fc85d89acc0f287516bf367efebecc7ea085808367f978f69fb8c2")]
 [assembly: InternalsVisibleTo("Rafy.Web, PublicKey=0024000004800000940000000602000000240000525341310004000001000100d3ae8c70610ec4e9ddd1c83c90c9e99c19a4c675e79b0360283797b567ea20e1a6804e1bd5459f5ad6d354a3ee5d1608faafff8d33460116af32c3e9b012c38f5ef334eff3b7f85f1a3db52e4f44ebbbf24429b5650072bb1968ea80dec1b97ab9429db053fc85d89acc0f287516bf367efebecc7ea085808367f978f69fb8c2")]
