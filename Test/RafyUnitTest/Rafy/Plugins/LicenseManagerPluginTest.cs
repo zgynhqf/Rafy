e141b4593286e50aedbede60c307ae2096920c22
Huqf <9474649@qq.com>
2017-08-12 16:10:18 +0800

    compile error

--- Test/RafyUnitTest/Rafy/Plugins/LicenseManagerPluginTest.cs
+++ Test/RafyUnitTest/Rafy/Plugins/LicenseManagerPluginTest.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Rafy.LicenseManager.Encryption;
 
-namespace RafyUnitTest.Rafy.Plugins
+namespace RafyUnitTest
 {
     /// <summary>
     /// 授权认证插件的单元测试类
