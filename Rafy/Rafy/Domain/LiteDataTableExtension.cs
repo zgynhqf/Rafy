be6e6eb0e7c0a9d12644717a062901f41b4adf0c
HK <hk@dbgo.cn>
2017-08-17 20:18:45 +0800

    完善 LiteDataTable 扩展方法

--- Rafy/Rafy/Domain/LiteDataTableExtension.cs
+++ Rafy/Rafy/Domain/LiteDataTableExtension.cs
@@ -54,6 +54,7 @@ namespace Rafy.Domain
 
             if (!columnMapToProperty)
             {
+                List<EntityPropertyMeta> pMetaList = new List<EntityPropertyMeta>();
                 IList<EntityPropertyMeta> entityPropertyMetaList = entityMeta.EntityProperties;
 
                 #region 判断 liteDataTable 类型能否转换成 EntityList 类型
@@ -61,13 +62,35 @@ namespace Rafy.Domain
                 List<string> entitycolumnNameList = new List<string>();
                 for (int i = 0; i < entityPropertyMetaList.Count; i++)
                 {
-                    var columnName = entityPropertyMetaList[i].ColumnMeta.ColumnName;
-                    entitycolumnNameList.Add(columnName);
+                    var propertyMeta = entityPropertyMetaList[i];
+                    var columnMeta = propertyMeta.ColumnMeta;
+
+                    var columnName = "";
+
+                    // 这个位置有时候会存在 columnMeta 为空的情况
+                    // 很多实体会默认继承 treeIndex  treePId  isphantom  
+                    // 而且这些属性没有映射到数据库，所以这个时候取 columnMeta 就是空的
+                    // 这个时候如果要转换我就不对这3个属性处理
+                    if (columnMeta != null)
+                    {
+                        //这个位置是针对于时间戳的那几个字段 columnMeta 不为空 但是 映射到数据库的 columnName 这个属性是空的
+                        if (columnMeta.ColumnName == null)
+                        {
+                            columnName = propertyMeta.Name;
+                        }
+                        else
+                        {
+                            columnName = columnMeta.ColumnName;
+                        }
+                        entitycolumnNameList.Add(columnName);
+                        pMetaList.Add(propertyMeta);
+                    }
                 }
 
                 for (int i = 0; i < entitycolumnNameList.Count; i++)
                 {
-                    if (!liteDataTableColumnsNameList.Contains(entitycolumnNameList[i]))
+                    var entityColumnName = entitycolumnNameList[i];
+                    if (!liteDataTableColumnsNameList.Contains(entityColumnName) && (!LiteDataTableExtension.IsInheritProperty(entityColumnName)))
                     {
                         throw new NotSupportedException("需要转换的 LiteDataTable 不包含 " + entitycolumnNameList[i] + " 列，所以不能转换");
                     }
@@ -78,11 +101,16 @@ namespace Rafy.Domain
                 foreach (var row in liteDataTable.Rows)
                 {
                     var entityItem = repo.New();
-                    foreach (var metaProperty in entityPropertyMetaList)
+                    foreach (var metaProperty in pMetaList)
                     {
                         var manageProperty = metaProperty.ManagedProperty;
                         var propertyName = metaProperty.ColumnMeta.ColumnName;
-                        entityItem.LoadProperty(manageProperty, row[propertyName]);
+                        if (propertyName == null)
+                        {
+                            propertyName = metaProperty.Name;
+                        }
+                        var rowValue = row[propertyName];
+                        entityItem.LoadProperty(manageProperty, rowValue);
                     }
                     resultList.Add(entityItem);
                 }
@@ -118,6 +146,25 @@ namespace Rafy.Domain
                     resultList.Add(entityItem);
                 }
                 return resultList as TEntityList;
+            }
+        }
+
+        /// <summary>
+        /// 判断属性是否为继承属性 treeIndex  treePId  isphantom
+        /// 很多实体会默认继承 treeIndex  treePId  isphantom 
+        /// 而且这些属性没有映射到数据库
+        /// 所以当类型转换的时候 ，就不需要判断这几个属性
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsInheritProperty(string propertyName)
+        {
+            if (propertyName == "IsPhantom" || propertyName == "TreeIndex" || propertyName == "TreePId")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
     }
