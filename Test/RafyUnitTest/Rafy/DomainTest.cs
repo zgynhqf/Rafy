c29073b328136cc86bfcb7f2fd0fd603afd7e3ec
HK <hk@dbgo.cn>
2017-08-17 20:17:16 +0800

    添加完善 liteDataTable 转 entitylist 的单元测试

--- Test/RafyUnitTest/Rafy/DomainTest.cs
+++ Test/RafyUnitTest/Rafy/DomainTest.cs
@@ -80,7 +80,7 @@ namespace RafyUnitTest
         }
 
         [TestMethod]
-        public void DT_ExtLiteDataTable_CanConvert()
+        public void DT_LiteDataTable_CanConvertToEntity()
         {
             var table = new LiteDataTable();
             table.Columns.Add(new LiteDataColumn("Id", typeof(int)));
@@ -112,8 +112,81 @@ namespace RafyUnitTest
             table.Rows.Add(row);
 
             var customer = table.ToEntityList<CustomerList>();
-            Assert.AreEqual(1, customer.Count, "liteDataTable 应该能转换成一条 customer 的数据");
+            Assert.AreEqual(1, customer.Count, "通过自己组装的 liteDataTable 应该能转换成一条 customer 的数据");
         }
 
+        /// <summary>
+        /// liteDateTable 没有继承属性也能转 entitylist 类型
+        /// </summary>
+        public void DT_LiteDataTable_CanConvertToEntity_NoInheritProperty()
+        {
+            var table = new LiteDataTable();
+            table.Columns.Add(new LiteDataColumn("Id", typeof(int)));
+            table.Columns.Add(new LiteDataColumn("Name", typeof(string)));
+            table.Columns.Add(new LiteDataColumn("DecimalProperty1", typeof(decimal)));
+            table.Columns.Add(new LiteDataColumn("DecimalProperty2", typeof(decimal)));
+            table.Columns.Add(new LiteDataColumn("DecimalProperty3", typeof(decimal)));
+            table.Columns.Add(new LiteDataColumn("CreatedTime", typeof(DateTime)));
+            table.Columns.Add(new LiteDataColumn("CreatedUser", typeof(string)));
+            table.Columns.Add(new LiteDataColumn("UpdatedTime", typeof(string)));
+            table.Columns.Add(new LiteDataColumn("UpdatedUser", typeof(DateTime)));
+            table.Columns.Add(new LiteDataColumn("IsPhantom", typeof(bool)));
+            table.Columns.Add(new LiteDataColumn("TreeIndex", typeof(int)));
+            table.Columns.Add(new LiteDataColumn("TreePId", typeof(int)));
+
+            var row = table.NewRow();
+            row["Id"] = 1;
+            row["Name"] = "HuKang";
+            row["DecimalProperty1"] = 1;
+            row["DecimalProperty2"] = 2;
+            row["DecimalProperty3"] = 3;
+            row["CreatedTime"] = DateTime.Now;
+            row["UpdatedTime"] = DateTime.Now;
+            row["CreatedUser"] = "hk";
+            row["UpdatedUser"] = "hk";
+            table.Rows.Add(row);
+
+            var customer = table.ToEntityList<CustomerList>();
+            Assert.AreEqual(1, customer.Count, "通过自己组装的且没有继承属性的 liteDataTable 应该能转换成一条 customer 的数据");
+        }
+
+        [TestMethod]
+        public void DT_LiteDataTable_QueryFromDbAndConvertToEntity()
+        {
+            CustomerRepository repo = RF.ResolveInstance<CustomerRepository>();
+            using (RF.TransactionScope(repo))
+            {
+                var customer = new Customer();
+                customer.DecimalProperty1 = 1;
+                customer.DecimalProperty2 = 2;
+                customer.DecimalProperty3 = 3;
+                customer.Name = "hk";
+                repo.Save(customer);
+
+                var table = repo.GetAllTable();
+                var customerEntity = table.ToEntityList<CustomerList>(false);
+                Assert.AreEqual(1, customerEntity.Count, "通过数据库查询出来的 liteDataTable 应该能转换成一条 customer 的数据");
+            }
+        }
+
+        /// <summary>
+        /// 继承属性映射到数据库，那么 liteDateTable 转换成 entitylist 值应该保持一致
+        /// </summary>
+        [TestMethod]
+        public void DT_LiteDataTable_QueryFromDbAndConvertToEntity_HasInheritProperty()
+        {
+            var repo = RF.ResolveInstance<InvoiceRepository>();
+            using (RF.TransactionScope(repo))
+            {
+                var invoice = new Invoice();
+                invoice.Code = "123";
+                invoice.SetIsPhantom(true);
+                repo.Save(invoice);
+
+                var table = repo.GetAllTable();
+                var invoiceEntity = table.ToEntityList<InvoiceList>(false);
+                Assert.AreEqual(true, invoiceEntity[0].GetIsPhantom(), "继承属性映射到数据库，那么 liteDateTable 转换成 entitylist 值应该保持一致");
+            }
+        }
     }
 }
