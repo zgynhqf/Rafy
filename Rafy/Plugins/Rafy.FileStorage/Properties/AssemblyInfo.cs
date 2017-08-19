eb077d3db6dab1124727f1d913eb78b62d22e408
Huqf <9474649@qq.com>
2017-08-19 18:35:01 +0800

    Rafy.FileStorage 升级到 Net Standard 2.0。

--- Rafy/Plugins/Rafy.FileStorage/Properties/AssemblyInfo.cs
+++ Rafy/Plugins/Rafy.FileStorage/Properties/AssemblyInfo.cs
@@ -5,22 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Rafy.FileStorage")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Rafy.FileStorage")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+//[assembly: AssemblyTitle("Rafy.FileStorage")]
+//[assembly: AssemblyDescription("")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyCompany("")]
+//[assembly: AssemblyProduct("Rafy.FileStorage")]
+//[assembly: AssemblyCopyright("Copyright ©  2013")]
+//[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+//[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("32d70ed9-fe63-4c32-8df1-92f34004ce11")]
+//[assembly: Guid("32d70ed9-fe63-4c32-8df1-92f34004ce11")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.51.3140")]
+//[assembly: AssemblyVersion("3.51.3140")]
 
 [assembly: InternalsVisibleTo("RafyUnitTest, PublicKey=0024000004800000940000000602000000240000525341310004000001000100d3ae8c70610ec4e9ddd1c83c90c9e99c19a4c675e79b0360283797b567ea20e1a6804e1bd5459f5ad6d354a3ee5d1608faafff8d33460116af32c3e9b012c38f5ef334eff3b7f85f1a3db52e4f44ebbbf24429b5650072bb1968ea80dec1b97ab9429db053fc85d89acc0f287516bf367efebecc7ea085808367f978f69fb8c2")]
