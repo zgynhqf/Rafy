2db7326184afcebb13006b2f24855fa78200f718
Huqf <9474649@qq.com>
2017-08-19 18:35:12 +0800

    删除无用的代码。

--- Rafy/Rafy/Properties/AssemblyInfo.cs
+++ Rafy/Rafy/Properties/AssemblyInfo.cs
@@ -17,10 +17,10 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+//[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a1e82c56-5431-42a4-bb0b-41fe93f4f9d1")]
+//[assembly: Guid("a1e82c56-5431-42a4-bb0b-41fe93f4f9d1")]
 
 // Version information for an assembly consists of the following four values:
 //
