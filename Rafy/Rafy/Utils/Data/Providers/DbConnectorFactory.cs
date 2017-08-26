6aa93b007acdda71faca7734f94cfc8609e89727
wang9275483 <9275483@qq.com>
2017-08-26 11:32:06 +0800

    #61 NetStandard2.0 代码规范

--- Rafy/Rafy/Utils/Data/Providers/DbConnectorFactory.cs
+++ Rafy/Rafy/Utils/Data/Providers/DbConnectorFactory.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿/*******************************************************
+ * 
+ * 作者：胡庆访
+ * 创建日期：20170826
+ * 说明：此文件只包含一个类，具体内容见类型注释。
+ * 运行环境：.NET 4.0
+ * 版本号：1.0.0
+ * 
+ * 历史记录：
+ * 创建文件 胡庆访 20170826 11:30
+ * 
+*******************************************************/
+
+using System;
 using System.Collections.Generic;
 using System.Data.Common;
 using System.Linq;
@@ -8,7 +21,7 @@ using System.Text.RegularExpressions;
 namespace Rafy.Data.Providers
 {
     /// <summary>
-    /// 根据提供的字符串类型数据库提供器转换类型，可以获取指定的DbProviderFactory和ISqlProvider类型的具体实例
+    /// 根据提供的字符串类型数据库提供器转换类型，可以获取指定的 DbProviderFactory 和 ISqlProvider 类型的具体实例
     /// </summary>
     internal class DbConnectorFactory
     {
@@ -19,7 +32,7 @@ namespace Rafy.Data.Providers
         /// 以快速键值对照来获取 DbProviderFactory。
         /// </summary>
         /// <param name="provider"></param>
-        /// <returns>返回DbProviderFactory类型的具体对象实例</returns>
+        /// <returns>返回 DbProviderFactory 类型的具体对象实例</returns>
         public static DbProviderFactory GetFactory(string provider)
         {
             switch (provider)
@@ -48,7 +61,7 @@ namespace Rafy.Data.Providers
         /// 创建指定的链接字符串的转换器
         /// </summary>
         /// <param name="provider">指定的数据库的提供程序</param>
-        /// <returns>返回针对指定数据库的ISqlProvider类型的具体对象实例</returns>
+        /// <returns>返回针对指定数据库的 ISqlProvider 类型的具体对象实例</returns>
         public static ISqlProvider Create(string provider)
         {
             //ISqlConverter Factory
@@ -62,7 +75,7 @@ namespace Rafy.Data.Providers
                 case DbSetting.Provider_SqlCe:
                     if (_sqlConverter == null) _sqlConverter = new SqlServerProvider();
                     return _sqlConverter;
-                //PatrickLiu增加的有关MySql的代码
+                // PatrickLiu 增加的有关 MySql 的代码
                 case DbSetting.Provider_MySql:
                     if (_mySqlConverter == null) _mySqlConverter = new MySqlServerProvider();
                     return _mySqlConverter;
