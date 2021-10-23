c2c7acc9f96e127f107c09105f493279cff651a9
HuQingfang <9474649@qq.com>
2021-10-24 05:59:38 +0800

    cmt

--- Rafy/Rafy/ManagedProperty/Serialization/Mobile/MobileObjectFormatter.cs
+++ Rafy/Rafy/ManagedProperty/Serialization/Mobile/MobileObjectFormatter.cs
@@ -12,14 +12,19 @@ using System.Text;
 namespace Rafy.Serialization.Mobile
 {
     /// <summary>
+    /// 使用 NetDataContractSerializer 则需要调用双方都使用 .NET；这在 Web、SilverLight 环境下都不可用。
+    /// 而 DataContractJsonSerializer、DataContractSerializer 在序列化和反序列化时，都需要知道类型信息。
+    /// 所以本命名空间下的类型，以一种“适配器”的模式，MobileObjectFormatter 来解决了上述问题：
+    /// * 统一了传输的类型 <see cref="SerializationInfoContainer"/>。
+    /// * 使用 DataContractJsonSerializer 来序列化和反序列化实体。
+    /// * 在 MobileObject 基类中，实现了统一的序列化和反序列化的回调。
+    /// 本实现，拷贝自 CSLA。
+    /// 
     /// Serializes and deserializes objects
     /// at the field level. A Silverlight-
     /// compatible facsimile of the
     /// BinaryFormatter or NetDataContractSerializer.
     /// </summary>
-#if TESTING
-  [System.Diagnostics.DebuggerStepThrough]
-#endif
     public sealed class MobileObjectFormatter : ISerializationFormatter
     {
         #region 实例门面接口
--- Rafy/Rafy/ManagedProperty/Serialization/Mobile/_ReadMe.txt
+++ Rafy/Rafy/ManagedProperty/Serialization/Mobile/_ReadMe.txt
@@ -1,1 +1,8 @@
-﻿此文件夹中的类主要用于实现 JSON 自定义序列化。
+﻿使用 NetDataContractSerializer 则需要调用双方都使用 .NET；这在 Web、SilverLight 环境下都不可用。
+而 DataContractJsonSerializer、DataContractSerializer 在序列化和反序列化时，都需要知道类型信息。
+所以本命名空间下的类型，以一种“适配器”的模式，MobileObjectFormatter 来解决了上述问题：
+* 统一了传输的类型：SerializationInfoContainer。
+* 使用 DataContractJsonSerializer 来序列化和反序列化实体。
+* 在 MobileObject 基类中，实现了统一的序列化和反序列化的回调。
+
+本实现，拷贝自 CSLA。
