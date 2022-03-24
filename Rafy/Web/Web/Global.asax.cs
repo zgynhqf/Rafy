c3a91087437d2c953ddd0c51c83ecb65706fde04
HuQingfang <9474649@qq.com>
2022-03-24 21:14:16 +0800

    初步修复 ExtWeb.

--- Rafy/Web/Web/Global.asax.cs
+++ Rafy/Web/Web/Global.asax.cs
@@ -75,17 +75,20 @@ namespace Rafy.Web.Site
         {
             protected override void OnRuntimeStarting()
             {
-                var svc = ServiceFactory.Create<MigrateService>();
-                svc.Options = new MigratingOptions
+                if (ConfigurationHelper.GetAppSettingOrDefault("Web.AutoUpdateDb", false))
                 {
-                    //ReserveHistory = true,//ReserveHistory 表示是否需要保存所有数据库升级的历史记录
-                    RunDataLossOperation = DataLossOperation.All,//要支持数据库表、字段的删除操作，取消本行注释。
-                    Databases = new string[] {
-                        DbSettingNames.RafyPlugins,
-                        "JXC"
-                    }
-                };
-                svc.Invoke();
+                    var svc = ServiceFactory.Create<MigrateService>();
+                    svc.Options = new MigratingOptions
+                    {
+                        //ReserveHistory = true,//ReserveHistory 表示是否需要保存所有数据库升级的历史记录
+                        RunDataLossOperation = DataLossOperation.All,//要支持数据库表、字段的删除操作，取消本行注释。
+                        Databases = new string[] {
+                            DbSettingNames.RafyPlugins,
+                            "JXC"
+                        }
+                    };
+                    svc.Invoke();
+                }
 
                 base.OnRuntimeStarting();
             }
--- Rafy/Web/Web/Properties/AssemblyInfo.cs
+++ Rafy/Web/Web/Properties/AssemblyInfo.cs
@@ -2,34 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Rafy.Web.Site")]
+[assembly: AssemblyTitle("Web")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Rafy.Web.Site")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyProduct("Web")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ed070333-5429-443a-be9e-3dcaefb5155e")]
+[assembly: Guid("0c8ff175-446a-4872-8436-3fc639248f02")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
